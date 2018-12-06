c7d9e81b3238370c1218cb3743b11d24e375f36c
pparaska <45350812+pparaska@users.noreply.github.com>
2018-12-06 18:05:58 +0530

    Delete proto.js

--- proto.js
+++
@@ -1,8 +0,0 @@
-var amitabh= {name:"Amitabh", surname:"Bacchan"}
-var abhishek= Object.create(amitabh);
-console.log(amitabh.name);
-console.log(amitabh.surname);
-console.log(__proto__);
-abhishek.name="Abhishek";
-console.log(abhishek.name);
-console.log(abhishek.surname);
