58c642f91b34f7e03cce74767c74cbf0d8913504
pparaska <45350812+pparaska@users.noreply.github.com>
2018-12-06 18:06:29 +0530

    Delete objectlit.js

--- objectlit.js
+++
@@ -1,9 +0,0 @@
-var person={name:"Poonam", name1:"Raj",id:101, add:"mumbai"}
-//console.log(person);
-console.log(person.name);
-console.log(person["name"]);
-console.log(person.id);
-console.log(person.add);
-console.log(person.name1);
-
-
